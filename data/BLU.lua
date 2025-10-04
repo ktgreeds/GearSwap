6fe1431a8053e172fe685eaee6f2803e86451aff
ktgreeds <kt.greeds@gmail.com>
2025-10-04 11:09:35 +0900

    Update BLU.lua

--- data/BLU.lua
+++ data/BLU.lua
@@ -17,7 +17,7 @@ function job_setup()
     state.IdleMode:options('Normal','Refresh')
     
     -- gs c cycle OffenseMode
-    state.OffenseMode:options('Normal','SubtleBlow')
+    state.OffenseMode:options('Normal','LockWeapons')
     
     -- gs c cycle HybridMode
     state.HybridMode:options('Normal')
@@ -32,9 +32,10 @@ function job_setup()
     state.SubWeapons    = M{'SakpatasSword'}
     
     -- gs c cycle TreasureHunter
-    state.TreasureHunter = M(false, 'TreasureHunter')
+    state.TreasureHunter = M(false)
+    -- gs c cycle LockWeapons
+    state.LockWeapons = M(false)
 end
-
 
 function job_post_midcast(spell, action, spellMap, eventArgs)
     if spell.skill == '青魔法' then
@@ -44,7 +45,64 @@ function job_post_midcast(spell, action, spellMap, eventArgs)
             end
         end
     end
+
+    if string.find(spell.type, 'Magic') then
+        local fc=73/100
+        if player.sub_job == '赤' then
+            fc = fc + 15/100
+        end
+        if fc >= 80/100 then
+            fc = 80/100
+        end
+
+        equip(sets.midcast.interruption)
+        local adjust=0.9
+        local cast_time = (spell.cast_time*(1-fc))*adjust
+
+        if spellMap == 'BlueMagical' then
+            eventArgs.handled = true
+            send_command('wait '..cast_time..'; gs equip sets.midcast.BlueMagical')
+
+        elseif spellMap == 'BlueMagicAcc' then
+            eventArgs.handled = true
+            send_command('wait '..cast_time..'; gs equip sets.midcast.BlueMagicAcc')
+
+        elseif spellMap == 'BlueHealing' then
+            eventArgs.handled = true
+            send_command('wait '..cast_time..'; gs equip sets.midcast.BlueHealing')
+
+        elseif spellMap == 'BlueBuff' then
+            eventArgs.handled = true
+            send_command('wait '..cast_time..'; gs equip sets.midcast.BlueBuff')
+        
+        elseif spell.name== 'アクアベール' then
+            eventArgs.handled = true
+            send_command('wait '..cast_time..'; gs equip sets.midcast.Aquaveil')        
+        end
+    end
+
     if state.TreasureHunter.value then
         equip(sets.TreasureHunter)
+    end
+end
+
+function customize_idle_set(idleSet)
+    idleSet = set_combine(idleSet, {main=gear.Nehushtan,sub=gear.Nehushtan})
+    return idleSet
+end
+
+function user_customize_melee_set(meleeSet)
+
+    return meleeSet
+end
+
+function job_state_change(stateField, newValue, oldValue)
+    if stateField == 'Offense Mode' then
+        if newValue == 'Normal' then
+            enable('main','sub')
+        else
+            equip({main=gear.Tizona,sub=gear.SakpatasSword})
+            disable('main','sub')
+        end
     end
 end
