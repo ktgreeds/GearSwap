1911677c708cec2afa61ee68be7cfa2241762c2e
ktgreeds <kt.greeds@gmail.com>
2025-10-12 09:33:39 +0900

    Update RUN.lua

--- data/RUN.lua
+++ data/RUN.lua
@@ -25,9 +25,6 @@ function job_setup()
 
     -- gs c cycle SubWeapons
     state.SubWeapons    = M{'Khonsu','AlberStrap'}
-
-    -- gs c cycle RuneText
-    state.RuneText = M(false, 'ルーン')
 end
 
 
@@ -105,10 +102,9 @@ function job_buff_change(buff, gain)
     end
 end
 
-
-function custom_self_command(cmdParams, eventArgs)
-    if cmdParams[1] =='RuneText' then --【剣】ルーンカンニングペーパ－
-        if state.RuneText.value then
+function job_state_change(stateField, newValue, oldValue)
+    if stateField == 'RuneText' then
+        if newValue then
             showTextRune()
         else
             hideTextRune()
@@ -117,34 +113,3 @@ function custom_self_command(cmdParams, eventArgs)
 end
 
 
---▼▼▼▼▼▼ルーンカンニングペーパー▼▼▼▼▼
-local RUnetexts = require('texts')
-message={
-    [1] = {name='イグニス　', desc1='火：',desc2='（氷：',desc3='麻痺、バインド）'},
-    [2] = {name='ゲールス　', desc1='氷：',desc2='（風：',desc3='静寂、ヘヴィ）'},
-    [3] = {name='フラブラ　', desc1='風：',desc2='（土：',desc3='石化、テラー、スロウ）'},
-    [4] = {name='テッルス　', desc1='土：',desc2='（雷：',desc3='スタン）'},
-    [5] = {name='スルポール', desc1='雷：',desc2='（水：',desc3='毒）'},
-    [6] = {name='ウンダ　　', desc1='水：',desc2='（火：',desc3='病気、悪疫、アムネジア）'},
-    [7] = {name='ルックス　', desc1='光：',desc2='（闇：',desc3='呪い、睡眠）'},
-    [8] = {name='テネブレイ', desc1='闇：',desc2='（光：',desc3='魅了、睡眠）'},
-}
-text_box = RUnetexts.new('${text}',{text={font='Meiryo', size=10}, pos={x=10, y=220}, padding = 5, bg={alpha=180}})
-text_box.text='属性　　　　　　耐性\n'..
-'\\cs(255, 204, 204)'..message[1].desc1..message[1].name..message[1].desc2..message[1].desc3..'\\cr'..'\n'..
-'\\cs(204, 255, 255)'..message[2].desc1..message[2].name..message[2].desc2..message[2].desc3..'\\cr'..'\n'..
-'\\cs(204, 255, 204)'..message[3].desc1..message[3].name..message[3].desc2..message[3].desc3..'\\cr'..'\n'..
-'\\cs(255, 255, 204)'..message[4].desc1..message[4].name..message[4].desc2..message[4].desc3..'\\cr'..'\n'..
-'\\cs(255, 204, 255)'..message[5].desc1..message[5].name..message[5].desc2..message[5].desc3..'\\cr'..'\n'..
-'\\cs(204, 204, 255)'..message[6].desc1..message[6].name..message[6].desc2..message[6].desc3..'\\cr'..'\n'..
-'\\cs(255, 255, 255)'..message[7].desc1..message[7].name..message[7].desc2..message[7].desc3..'\\cr'..'\n'..
-'\\cs(204, 204, 204)'..message[8].desc1..message[8].name..message[8].desc2..message[8].desc3..'\\cr'
-
-function showTextRune()
-    text_box:show()
-end
-
-function hideTextRune()
-    text_box:hide()
-end
---▲▲▲▲▲ルーンカンニングペーパー▲▲▲▲▲
