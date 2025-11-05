dd0ba2915601f0ba10459c91fb4321ecbeeff788
ktgreeds <kt.greeds@gmail.com>
2025-11-05 17:06:08 +0900

    Update weapon and gear settings in SAM_gear.lua

--- data/メインキャラ/SAM_gear.lua
+++ data/メインキャラ/SAM_gear.lua
@@ -1,7 +1,6 @@
 function init_weaponns()
     -- デフォルト武器を設定
-    send_command('gs c set MainWeapons DojikiriYasutsuna')
-    send_command('gs c set SubWeapons Khonsu')
+    send_command('gs c set MainWeapons DojikiriYasutsuna; wait 0.3; gs c set SubWeapons UtuGrip')
 end
 
 
@@ -19,15 +18,18 @@ function init_gear_sets()
     gear.UtuGrip                = {name="ウトゥグリップ"}
     gear.Khonsu                 = {name="コーンスー"}
     
+    -- その他
+    gear.Slip                   = {name="原始刀"}
+    
     -- 監視用バフ
     sets.buff['八双']           = {}
     sets.buff['星眼']           = {}
-    sets.buff['心眼']           = {}
+    sets.buff['心眼']           = {legs="極左近士板佩楯"}
     
     -- 待機装備
     sets.idle = {
         ammo="ストンチタスラム+1",
-        head={ name="ニャメヘルム", augments={'Path: B',}},
+        head="無の面",
         body="アダマンアーマー",
         hands={ name="ニャメガントレ", augments={'Path: B',}},
         legs={ name="ニャメフランチャ", augments={'Path: B',}},
@@ -43,12 +45,12 @@ function init_gear_sets()
 
     -- 抜刀装備
     sets.engaged = {
-        ammo={ name="コイストボダー", augments={'Path: A',}},
+        ammo="オゲルミルオーブ",
         head="極春日烏帽子形兜",
         body="極春日胴丸",
         hands={ name="楯無篭手改", augments={'Path: A',}},
-        legs={ name="楯無佩楯改", augments={'Path: A',}},
-        feet={ name="楯無脛当改", augments={'Path: A',}},
+        legs="極春日板佩楯",
+        feet={ name="龍王脛当改", augments={'HP+65','"Store TP"+5','"Subtle Blow"+8',}},
         neck="月光の喉輪",
         waist={ name="ケンタークベルト+1", augments={'Path: A',}},
         left_ear="アスプロピアス",
@@ -60,63 +62,31 @@ function init_gear_sets()
 
     -- 抜刀装備　八双 : OffenseMode=Hasso  HybridMode=Normal
     sets.engaged.Hasso = set_combine(sets.engaged,{
-        feet="越脇戸筒脛当"
+        Legs="極春日板佩楯"
     })
     
     -- 抜刀装備　星眼 : OffenseMode=Seigan HybridMode=Normal
     sets.engaged.Seigan = set_combine(sets.engaged,{
         head="極春日烏帽子形兜",
         body="ムパカダブレット",
-        hands="ムパカグローブ",
-        back={ name="スメルトリオマント", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Store TP"+10','Damage taken-5%',}},
-    })
-    
-    -- 抜刀装備　心眼 : OffenseMode=Normal HybridMode=ThirdEye
-    sets.engaged.ThirdEye = set_combine(sets.engaged,{
-        legs="極左近士板佩楯"
-    })
-    
-    -- 抜刀装備　八双心眼 : OffenseMode=Hasso  HybridMode=ThirdEye
-    sets.engaged.Hasso.ThirdEye = set_combine(sets.engaged.Hasso,{
-        legs="極左近士板佩楯"
-    })
-    
-    -- 抜刀装備　星眼心眼 : OffenseMode=Seigan HybridMode=ThirdEye
-    sets.engaged.Seigan.ThirdEye = set_combine(sets.engaged.Seigan,{
-        legs="極左近士板佩楯"
-    })
-    
-    -- 抜刀装備　モクシャ62(42,20) : OffenseMode=Normal HybridMode=SubtleBlow
-    sets.engaged.SubtleBlow = set_combine(sets.engaged,{
-        neck={ name="バーシチョーカー+1", augments={'Path: A',}},--11
-        body="デーゴンブレスト",--II 10
-        legs="ムパカホーズ",--II 5
-        feet="乾闥婆脛当改",--8
-        waist="サリサフロイベルト",--5
-        left_ear="ディグニタリピアス",--5
-        right_ear="シェレピアス",--3
-        left_ring="ニックマドゥリング",--II 5
-        right_ring="シーリチリング+1",--10
-    })
+        back={ name="スメルトリオマント", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Store TP"+10','System: 1 ID: 640 Val: 4',}},
+     })
 
-    -- 即時発動アビリティ　明鏡止水
+    -- 即時発動アビリティ
     sets.precast.JA['明鏡止水'] = {
         legs="左近士筒脛当改"
     }
     
-    -- 即時発動アビリティ　黙想
     sets.precast.JA['黙想'] = {
         head="極脇戸桃形兜",
         hands="極左近士筒篭手",
         back="スメルトリオマント"
     }
 
-    -- 即時発動アビリティ　先義後利
     sets.precast.JA['先義後利'] = {
         legs="極春日筒脛当"
     }
 
-    -- 即時発動アビリティ　護摩の守護円
     sets.precast.JA['護摩の守護円'] = {
         head="極脇戸桃形兜"
     }
@@ -166,20 +136,6 @@ function init_gear_sets()
         left_ring="スローダリング",
         right_ring="王将の指輪",
         back={ name="スメルトリオマント", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%','Phys. dmg. taken-10%',}},
-    }
-
-    -- WSモクシャ62(42,20)
-    sets.precast.WS.SubtleBlow = 
-    {
-        neck={ name="バーシチョーカー+1", augments={'Path: A',}},--11
-		body="デーゴンブレスト",--II 10
-        legs="ムパカホーズ",--II 5
-        feet="乾闥婆脛当改",--8
-        waist="サリサフロイベルト",--5
-        left_ear="ディグニタリピアス",--5
-        right_ear="シェレピアス",--3
-        left_ring="ニックマドゥリング",--II 5
-        right_ring="シーリチリング+1",--10
     }
     
     --共通WS定義読み込み
