a60ce477a76290d780c46255e697397cfe297d51
ktgreeds <kt.greeds@gmail.com>
2025-11-14 14:30:19 +0900

    Update user-globals.lua

--- data/user-globals.lua
+++ data/user-globals.lua
@@ -72,9 +72,8 @@ function user_post_midcast(spell, action, spellMap, eventArgs)
         
         eventArgs.handled = true
         
-        local adjust = 0.07
+        local adjust = 0.1
         local cast_time = (spell.cast_time*(1-fc))-adjust
-
         IdleMelee()
         equip(sets.midcast.interruption)
 
