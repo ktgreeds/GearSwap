ab767cd4a55242594ad1143287007399f0520e3d
ktgreeds <kt.greeds@gmail.com>
2025-11-14 17:56:10 +0900

    Update BLU.lua

--- data/BLU.lua
+++ data/BLU.lua
@@ -42,65 +42,57 @@ function job_post_precast(spell, action, spellMap, eventArgs)
 end
 
 function job_post_midcast(spell, action, spellMap, eventArgs)
-    if spell.skill == '青魔法' then
-        for buff,active in pairs(state.Buff) do
-            if active and sets.buff[buff] then
-                equip(sets.buff[buff])
-            end
+    for buff,active in pairs(state.Buff) do
+        if active and sets.buff[buff] then
+            equip(sets.buff[buff])
         end
     end
 
     if string.find(spell.type, 'Magic') then
-        local fc=80/100
+        if not sets.precast.FC.value then
+            sets.precast.FC.value = 80
+        end
+
+        local fc = sets.precast.FC.value/100
+        if player.main_job == '剣' and buffactive['ファストキャスト'] then
+            --フサルクトラウザ装備時メリポのインスパイア1につき+12％
+            fc = fc + 12/100 --インスパイア1
+            --fc = fc + 24/100 --インスパイア2
+            --fc = fc + 36/100 --インスパイア3
+            --fc = fc + 48/100 --インスパイア4
+            --fc = fc + 60/100 --インスパイア5
+        end
         if player.sub_job == '赤' then
             fc = fc + 15/100
+        elseif player.main_job == '赤' then
+            fc = fc + 38/100
         end
         if fc >= 80/100 then
             fc = 80/100
         end
         
-        local adjust=0.8
-        local cast_time = (spell.cast_time*(1-fc))*adjust
         eventArgs.handled = true
-
-        if spell.cast_time > 1 then
-            equip(sets.midcast.interruption)
-        end
-
-        if spellMap == 'BlueMagical' then
-            if spell.name == 'エントゥーム' then
-                send_command('wait '..cast_time..'; gs equip sets.midcast['..windower.to_shift_jis('エントゥーム')..']')
-            elseif spell.name == 'テネブラルクラッシュ' then
-                send_command('wait '..cast_time..'; gs equip sets.midcast['..windower.to_shift_jis('テネブラルクラッシュ')..']')
-            elseif spell.name == 'アンビルライトニング' then
-                send_command('wait '..cast_time..'; gs equip sets.midcast['..windower.to_shift_jis('アンビルライトニング')..']')
-            else
-                send_command('wait '..cast_time..'; gs equip sets.midcast.BlueMagical')
-            end
-
-        elseif spellMap == 'BlueMagicAcc' then
-            if spell.name == '夢想花' then
-                send_command('wait '..cast_time..'; gs equip sets.midcast['..windower.to_shift_jis('夢想花')..']')
-            else
-                send_command('wait '..cast_time..'; gs equip sets.midcast.BlueMagicAcc')
-            end
-
-        elseif spellMap == 'BlueHealing' then
-            send_command('wait '..cast_time..'; gs equip sets.midcast.BlueHealing')
-
-        elseif spellMap == 'BlueBuff' then
-            send_command('wait '..cast_time..'; gs equip sets.midcast.BlueBuff')
         
-        elseif spell.name== 'アクアベール' then
-            send_command('wait '..cast_time..'; gs equip sets.midcast['..windower.to_shift_jis('アクアベール')..']')
+        local adjust = 0.15
+        local cast_time = (spell.cast_time*(1-fc))-adjust
 
-        elseif spellMap == 'Phalanx' then
-            send_command('wait '..cast_time..'; gs equip sets.midcast['..windower.to_shift_jis('ファランクス')..']')
+        IdleMelee()
+        equip(sets.midcast.interruption)
+
+        if sets.midcast[spell.name] then
+            send_command('wait '..cast_time..'; gs equip sets.midcast['..windower.to_shift_jis(spell.name)..']')
+        elseif sets.midcast[spellMap] then
+            send_command('wait '..cast_time..'; gs equip sets.midcast.'..spellMap)
+
             
-        elseif state.TreasureHunter.value then
-            equip(sets.TreasureHunter)
+        elseif sets.midcast[spell.skill] then
+            send_command('wait '..cast_time..'; gs equip sets.midcast['..windower.to_shift_jis(spell.skill)..']')
+
+
+        elseif sets.midcast[spell.type] then
+            send_command('wait '..cast_time..'; gs equip sets.midcast.'..spell.type) 
         end
-    end 
+    end
 end
 
 function customize_idle_set(idleSet)
