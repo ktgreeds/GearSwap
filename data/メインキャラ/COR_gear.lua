c26c3a7785dbe1c08237c5ec8caa0ff4b7cbf415
ktgreeds <kt.greeds@gmail.com>
2026-02-22 09:51:27 +0900

    Update COR_gear.lua

--- data/メインキャラ/COR_gear.lua
+++ data/メインキャラ/COR_gear.lua
@@ -34,6 +34,7 @@ function init_gear_sets()
     gear.MarksmanshipPhysics        = {name="クロノブレット"}
     gear.MarksmanshipMagical        = {name="ライヴブレット"}
     gear.HauksbokBullet             = {name="ホクスボクブレット"}
+    gear.HoxneAmpulla               = {name="ホクスニアムプラ"}
     gear.AnimikiiBullet             = {name="アニミキーブレット"}
     gear.CorsairShot                = gear.AnimikiiBullet 
     gear.LuzafsRing                 = {name="ルザフリング"}
@@ -77,10 +78,10 @@ function init_gear_sets()
         legs="ＣＳトルーズ+3",
         feet="マリグナスブーツ",
         neck="無の喉輪",
-        waist="セールフィベルト+1",
-        left_ear="ディグニタリピアス",
+        waist="ウィンバフベルト+1",
+        left_ear="アスプロピアス",
         right_ear="テロスピアス",
-        left_ring="シーリチリング+1",
+        left_ring="メランリング",
         right_ring="シーリチリング+1",
         back={ name="カムラスマント", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Store TP"+10','Phys. dmg. taken-10%',}},
     }
@@ -172,21 +173,21 @@ function init_gear_sets()
         back="ガンスリンガマント"       --敵対心-1
     }
     
-    sets.precast.CorsairRoll['プリッツアロール'] = {
+    sets.precast.CorsairRoll['プリッツアロール'] = set_combine(sets.precast.CorsairRoll,{
         head="ＣＳトリコルヌ+3",
-    }
-    sets.precast.CorsairRoll['タクティックロール'] = {
+    })
+    sets.precast.CorsairRoll['タクティックロール'] = set_combine(sets.precast.CorsairRoll,{
         body="ＣＳフラック+3",
-    }
-    sets.precast.CorsairRoll['アライズロール'] = {
+    })
+    sets.precast.CorsairRoll['アライズロール'] = set_combine(sets.precast.CorsairRoll,{
         hands="ＣＳガントリー+3",
-    }
-    sets.precast.CorsairRoll['キャスターズロール'] = {
+    })
+    sets.precast.CorsairRoll['キャスターズロール'] = set_combine(sets.precast.CorsairRoll,{
         Legs="ＣＳトルーズ+3",
-    }
-    sets.precast.CorsairRoll['コアサーズロール'] = {
+    })
+    sets.precast.CorsairRoll['コアサーズロール'] = set_combine(sets.precast.CorsairRoll,{
         feet="ＣＳブーツ+3",
-    }
+    })
     sets.precast.CorsairRoll['ボルターズロール'] = {
         main={ name="ロスタム", augments={'Path: C',}},
     }
@@ -198,15 +199,15 @@ function init_gear_sets()
 
     -- WSダメージ
     sets.precast.WS.Damage = {
-        head={ name="ニャメヘルム", augments={'Path: B',}},
-        body={ name="ニャメメイル", augments={'Path: B',}},
-        hands={ name="ニャメガントレ", augments={'Path: B',}},
-        legs={ name="ニャメフランチャ", augments={'Path: B',}},
-        feet={ name="ニャメソルレット", augments={'Path: B',}},
-        neck="共和プラチナ章",
-        waist={ name="セールフィベルト+1", augments={'Path: A',}},
-        left_ear={ name="胡蝶のイヤリング", augments={'Accuracy+4','TP Bonus +250',}},
-        right_ear="テロスピアス",
+        head="ニャメヘルム",
+        body="ニャメメイル",
+        hands="ニャメガントレ",
+        legs="ニャメフランチャ",
+        feet="ニャメソルレット",
+        neck={ name="コモドアチャーム+2", augments={'Path: A',}},
+        waist="セールフィベルト+1",
+        left_ear="胡蝶のイヤリング",
+        right_ear={ name="シャスーピアス+2", augments={'System: 1 ID: 1676 Val: 0','Accuracy+17','Mag. Acc.+17','Crit.hit rate+6','STR+9 AGI+9',}},
         left_ring="スローダリング",
         right_ring="エパミノダスリング",
         back={ name="カムラスマント", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%','Phys. dmg. taken-10%',}},
@@ -233,7 +234,8 @@ function init_gear_sets()
     sets.precast.WS.Magic = {
         ammo=gear.MarksmanshipMagical,
         head={ name="ニャメヘルム", augments={'Path: B',}},
-        body={ name="ニャメメイル", augments={'Path: B',}},
+        --body={ name="ニャメメイル", augments={'Path: B',}},
+        body="ＬＡフラック+3",
         hands={ name="ニャメガントレ", augments={'Path: B',}},
         legs={ name="ニャメフランチャ", augments={'Path: B',}},
         feet={ name="ＬＡブーツ+4", augments={'Enhances "Wild Card" effect',}},
@@ -250,8 +252,7 @@ function init_gear_sets()
     init_weapon_skill()
     
     --個別WS定義
-    sets.precast.WS["レデンサリュート"] = { Normal=set_combine(sets.precast.WS.Magic,{head="妖蟲の髪飾り+1",left_ring="アルコンリング"}),
-                                            SubtleBlow=set_combine(set_combine(sets.precast.WS.Magic,{head="妖蟲の髪飾り+1",left_ring="アルコンリング"}),sets.precast.WS.SubtleBlow )}
+    sets.precast.WS["レデンサリュート"] = set_combine(sets.precast.WS.Magic,{head="妖蟲の髪飾り+1",right_ring="アルコンリング"})
     sets.precast.WS["イオリアンエッジ"] = { Normal=set_combine(sets.precast.WS.Magic,{ammo=gear.HauksbokBullet}),
                                             SubtleBlow=set_combine(set_combine(sets.precast.WS.Magic,{ammo=gear.HauksbokBullet}),sets.precast.WS.SubtleBlow )}
     sets.precast.WS["エクゼンテレター"] = { Normal      = set_combine(sets.engaged.ACC),
@@ -278,4 +279,22 @@ function init_gear_sets()
     sets.midcast.CorsairShot = set_combine(sets.midcast.RA,{
         ammo=gear.CorsairShot,
     })
+
+    --AFと王将に変える
+    sets.midcast['ライトショット'] = {
+        range="デスペナルティ",
+        ammo=gear.CorsairShot,
+        head="ＬＫトリコルヌ+4",
+        body="ＬＫフラック+4",
+        hands="ＬＫガントリー+4",
+        legs="ＣＳトルーズ+3",
+        feet="ＬＫブーツ+4",
+        neck="無の喉輪",
+        waist="無の腰当",
+        left_ear="アスプロピアス",
+        right_ear={ name="シャスーピアス+2", augments={'System: 1 ID: 1676 Val: 0','Accuracy+17','Mag. Acc.+17','Crit.hit rate+6','STR+9 AGI+9',}},
+        left_ring="王将の指輪",
+        right_ring="メダダリング",
+        back="無の外装",
+    }
 end
