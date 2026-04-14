a1b43b1863e415ff225bf0f96e17c50198cf5eaf
ktgreeds <kt.greeds@gmail.com>
2026-04-15 07:12:03 +0900

    Update COR_gear.lua

--- data/メインキャラ/COR_gear.lua
+++ data/メインキャラ/COR_gear.lua
@@ -138,7 +138,7 @@ function init_gear_sets()
         left_ear="エテオレートピアス",
         right_ear="エンチャンピアス+1",
         left_ring="キシャールリング",
-        right_ring="メダダリング",
+        right_ring="ラハブリング",
         back="無の外装",
     }
 
@@ -239,8 +239,8 @@ function init_gear_sets()
         waist="オルペウスサッシュ",
         left_ear={ name="胡蝶のイヤリング", augments={'Accuracy+4','TP Bonus +250',}},
         right_ear="フリオミシピアス",
-        left_ring="ディンジルリング",
-        right_ring="メダダリング",
+        left_ring="コーネリアリング",
+        right_ring="ディンジルリング",
         back=gear.JobMantle.WSD.Magic
     }
     
@@ -294,7 +294,7 @@ function init_gear_sets()
         left_ear="アスプロピアス",
         right_ear={ name="シャスーピアス+2", augments={'System: 1 ID: 1676 Val: 0','Accuracy+17','Mag. Acc.+17','Crit.hit rate+6','STR+9 AGI+9',}},
         left_ring="王将の指輪",
-        right_ring="メダダリング",
+        right_ring="メタモルリング+1,
         back="無の外装",
     }
     
