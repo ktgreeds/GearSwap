8dac151944e991f418854c311a5a6135b595f6b1
ktgreeds <kt.greeds@gmail.com>
2026-05-11 09:03:02 +0900

    Refactor job setup and buff change functions

--- data/SAM.lua
+++ data/SAM.lua
@@ -5,7 +5,6 @@ function get_sets()
 end
 
 
-
 function job_setup()
     --buff
     state.Buff['八双'] = buffactive['八双'] or false
@@ -15,20 +14,28 @@ function job_setup()
     --state
     state.OffenseMode:options('Normal','SubtleBlow')
     state.WeaponskillMode:options('Normal','SubtleBlow')
-    state.MainWeapons = M{'DojikiriYasutsuna','ShiningOne'}
-    state.SubWeapons = M{'UtuGrip'}
-    state.Increased   = M(true)
+    state.MainWeapons   = M{'DojikiriYasutsuna','ShiningOne'}
+    state.SubWeapons    = M{'UtuGrip'}
+    state.Increased     = M(true)
+    state.HoxneAmpulla  = M(false)
 end
-
 
 
 function user_customize_melee_set(meleeSet)
     if state.Buff['心眼'] then
         meleeSet = set_combine(meleeSet,sets.buff['心眼'] )
+    elseif state.HoxneAmpulla then
+        if state.HoxneAmpulla.value then
+            equip({ammo="ホクスニアムプラ"})
+            meleeSet = set_combine(meleeSet,sets.engaged.HoxneAmpulla)
+            disable('ammo')
+        else
+            enable('ammo')
+            state.CombatForm:reset()
+        end
     end
     return meleeSet
 end 
-
 
 
 function job_state_change(stateField,  newValue, oldValue)
@@ -42,27 +49,25 @@ function job_state_change(stateField,  newValue, oldValue)
 end
 
 
-
 function job_buff_change(buff, gain)
-    if state.Buff['八双']  and state.Buff['星眼'] then
-        if buff == '八双' and gain then
-            state.CombatForm:set('八双')--星眼から八双へ
-        elseif buff == '星眼' and gain then
-            state.CombatForm:set('星眼')--八双から星眼へ
+    if buff=='八双' then
+        if gain then
+            state.CombatForm:set('八双')
+        else
+            if not state.Buff['星眼'] then
+                state.CombatForm:reset()
+            end
         end
-    elseif not state.Buff['八双']  and not state.Buff['星眼'] then
-        state.CombatForm:reset()        --八双星眼なし
-    elseif buff == '八双' and gain then
-        state.CombatForm:set('八双')    --八双単発
-    elseif buff == '星眼' and gain then
-        state.CombatForm:set('星眼')    --星眼単発
+        IdleMelee()
+
+    elseif buff=='星眼' then
+        if gain then
+            state.CombatForm:set('星眼')
+        else
+            if not state.Buff['八双'] then
+                state.CombatForm:reset()
+            end
+        end
+        IdleMelee()
     end
-    
-    if state.Buff['エンチャント'] then
-        state.CombatForm:set('エンチャント')
-        disable('range','ammo')
-    elseif not state.Buff['エンチャント']  then
-        enable('range','ammo')
-    end
-    IdleMelee()
 end
